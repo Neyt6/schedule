24f34590b431fb107b6b7eb33b3510950f552986
Антон Савенков <neyt6@ya.ru>
2024-09-11 16:35:52 +0300

    123

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Schedule />} />
+        <Route path="/schedule" element={<Schedule />} />
       </Routes>
     </Router>
   );
