be84068c45d9b8bab740e77d05cda943a96d7954
Антон Савенков <neyt6@ya.ru>
2024-09-11 16:38:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/schedule" element={<Schedule />} />
+        <Route path="/" element={<Schedule />} />
       </Routes>
     </Router>
   );
